371b0d60dbbd11838eb9c3d76fd92cbeec0982a0
LordFinn <176671863+LordFinn@users.noreply.github.com>
2024-11-06 18:24:01 +0100

    Add sorting algorithms which are to be implemented

--- src/SortingMachine.java
+++ src/SortingMachine.java
@@ -1,5 +1,24 @@
 public class SortingMachine {
 
-    // implement different sorting algorithms here
+    private static void swap(int[] array, int firstIndex, int secondIndex) {
+        int temp = array[firstIndex];
+        array[firstIndex] = array[secondIndex];
+        array[secondIndex] = temp;
+    }
+
+    public int[] bubbleSort(int[] array) {
+        // TODO: implement algorithm
+        return null;
+    }
+
+    public int[] cubeSort(int[] array) {
+        // TODO: implement algorithm
+        return null;
+    }
+
+    public int[] expoSort(int[] array) {
+        // TODO: implement algorithm
+        return null;
+    }
 
 }
