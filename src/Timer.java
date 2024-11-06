020281fd45fe2fe0fabc9fbbe88b891c639f669a
LordFinn <176671863+LordFinn@users.noreply.github.com>
2024-11-06 20:41:43 +0100

    Add Timer functionality

--- src/Timer.java
+++ src/Timer.java
@@ -1,5 +1,25 @@
+import java.util.Arrays;
+import java.util.function.Consumer;
+
 public class Timer {
 
-    // do timing stuff here
+    public static <T> long timeOnce(Consumer<T> function, T value) {
+        long startTime = System.nanoTime();
+        function.accept(value);
+        long endTime = System.nanoTime();
+        return endTime - startTime;
+    }
+
+    // works, but not useful for testing sorting algorithms, since an array will be sorted after the first timing
+    public static <T> long timeAverage(Consumer<T> function, T value, int howMany) {
+        if (howMany < 1) {
+            return 0;
+        }
+        long[] timings = new long[howMany];
+        for (int i = 0; i < howMany; i++) {
+            timings[i] = timeOnce(function, value);
+        }
+        return Arrays.stream(timings).sum() / howMany;
+    }
 
 }
