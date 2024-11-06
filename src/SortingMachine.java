34b64d565de049dedf6369fcd22f937a1c8a3f02
LordFinn <176671863+LordFinn@users.noreply.github.com>
2024-11-06 20:22:46 +0100

    Add expoSort implementation

--- src/SortingMachine.java
+++ src/SortingMachine.java
@@ -1,24 +1,42 @@
+import java.util.Random;
+
 public class SortingMachine {
 
-    private static void swap(int[] array, int firstIndex, int secondIndex) {
+    private static void swap(Integer[] array, int firstIndex, int secondIndex) {
         int temp = array[firstIndex];
         array[firstIndex] = array[secondIndex];
         array[secondIndex] = temp;
     }
 
-    public int[] bubbleSort(int[] array) {
-        // TODO: implement algorithm
-        return null;
+    public static Integer[] getRandomIntegerArray(int size) {
+        Random random = new Random();
+        Integer[] array = new Integer[size];
+        for (int i = 0; i < size; i++) {
+            array[i] = random.nextInt(1000);
+        }
+        return array;
     }
 
-    public int[] cubeSort(int[] array) {
+    public static void bubbleSort(Integer[] array) {
         // TODO: implement algorithm
-        return null;
     }
 
-    public int[] expoSort(int[] array) {
+    public static void cubeSort(Integer[] array) {
         // TODO: implement algorithm
-        return null;
+    }
+
+    public static void expoSort(Integer[] array) {
+        expoSort(array, array.length - 1);
+    }
+
+    private static void expoSort(Integer[] array, int to) {
+        if (to > 0) {
+            expoSort(array, to - 1);
+            if (array[to - 1] > array[to]) {
+                swap(array, to - 1, to);
+            }
+            expoSort(array, to - 1);
+        }
     }
 
 }
