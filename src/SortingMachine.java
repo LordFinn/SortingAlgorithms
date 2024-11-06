d9812f30693979bd0152b72542d8d55f2fff19b1
LordFinn <176671863+LordFinn@users.noreply.github.com>
2024-11-06 20:32:32 +0100

    Add bubbleSort implementation

--- src/SortingMachine.java
+++ src/SortingMachine.java
@@ -18,7 +18,13 @@ public class SortingMachine {
     }
 
     public static void bubbleSort(Integer[] array) {
-        // TODO: implement algorithm
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = array.length - 1; j > i; j--) {
+                if (array[j] < array[j - 1]) {
+                    swap(array, j, j - 1);
+                }
+            }
+        }
     }
 
     public static void cubeSort(Integer[] array) {
